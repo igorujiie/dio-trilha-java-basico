3ba488850cf5462688f4384a39e0c36b459c741a
Igor ujiie <igorkazuhico@hotmail.com>
2024-05-14 01:03:23 -0300

    subindo desafio conntador

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+       super("O primeiro parâmetro é maior que o segundo");
+        }
+    }
+
