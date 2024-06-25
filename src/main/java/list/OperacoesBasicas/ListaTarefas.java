bb3cf520d65c0b79097d7005f6f9de6fbc9d8b84
Igor ujiie <igorkazuhico@hotmail.com>
2024-06-24 23:49:25 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Igor ujiie <igorkazuhico@hotmail.com>

---
+++ src/main/java/list/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,52 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+        System.out.println("Número total de tarefas: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.adicionarTarefa("Estudar Java");
+        listaTarefas.adicionarTarefa("Estudar Java");
+        listaTarefas.adicionarTarefa("Estudar Python");
+        listaTarefas.adicionarTarefa("Estudar ");
+        System.out.println("Número total de tarefas: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.removerTarefa("Estudar Java");
+        System.out.println("Número total de tarefas: " + listaTarefas.obterNumeroTotalTarefas());
+
+        listaTarefas.obterDescricoesTarefas();
+
+    }
+}
---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,22 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    private String descricao;
+
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                '}';
+    }
+}
